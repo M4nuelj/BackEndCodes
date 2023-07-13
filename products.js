0eb6b527a65fd4cccc46b09b38d789eaa22f976b
M4nuelj <108774676+M4nuelj@users.noreply.github.com>
2023-07-13 14:34:16 +0000

    second change

--- products.js
+++ products.js
@@ -20,7 +20,7 @@ class ProductManager{
         }
 
     }
-    async addProduct(productList) {
+    addProduct(productList) {
         const {
             title,
             description,
@@ -41,20 +41,60 @@ class ProductManager{
 
             this.product.push(product);
             const stringFile=JSON.stringify(this.product, null, 2);
-            fs.writeFileSync("productManager.Json", stringFile);
+            fs.writeFileSync("productManager.json", stringFile);
+            return `We added the product "${product.title}" successfully`
 
         }else{
-            console.log("This product already exist, please verify the information and try again")
-            return
+            return `The product "${codeProduct.title}" already exist, please verify the information and try again`
         }
-
         
     }
 
+    getAllProducts(){
+        console.log(this.product)
+        return 
+    };
 
+    getProduct(id){
+
+        let findProduct= this.product.find(product=>product.id===id);
+        if(!findProduct){
+            return "El producto ingresado no existe"
+        }else{
+            return findProduct;
+        }
+
+    }
+    updateProduct(id, change){
+
+        let findProduct= this.product.findIndex(product=>product.id===id);
+        if(findProduct===-1){
+            return 'The product that you are requesting for does not exist, plase try again'
+        }
+        
+        let newProduct=this.product[findProduct];
+        let updatedProduct={...newProduct, ...change};
+        this.newProduct[findProduct]=updatedProduct;
+        fs.writeFileSync('productManager.json', JSON.stringify(this.product, null, 2));
+
+        return updatedProduct;
+
+    }
+    deleteProduct(id){
+        let findProduct= this.product.findIndex(x=>x.id===id);
+        if(findProduct===-1){
+            return 'This product does not exist'
+        }
+        let deletedProduct=this.product.filter(x=>x.id!==id)
+        fs.writeFileSync('productManager.json', JSON.stringify(deletedProduct, null, 2));
+        return `the product ${this.product[findProduct].title} was deleted`
+    }
 
 };
 
 const productManager =new ProductManager();
 // productManager.addProduct({ title:'Mango', description:'Fruit', price:2000, thumbnail:'NA', code:'abd234', stock:45});
-// productManager.addProduct({ title:'Apple', description:'Fruit', price:3000, thumbnail:'NA', code:'abd444', stock:48  });
+// console.log(productManager.addProduct({ title:'Apple', description:'Fruit', price:3000, thumbnail:'NA', code:'abd444', stock:48  }))
+// productManager.getAllProducts()
+// console.log(productManager.getProduct(7819587));
+// console.log(productManager.deleteProduct(9041528));
